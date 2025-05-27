99a8bbfa1c7d111968c2458744409d742edd13de
tirthpatel254 <heteshpatel17@gmail.com>
2025-05-27 16:55:41 +0530

    Add files via upload

---
+++ pr 4.7.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	
+	for(a=1;a<=5;a++)
+	{
+		if(a==1||a==3)
+		{
+			printf("* * * * *\n");
+		}else if(a==2)
+		{
+			printf("*       *\n");
+		}else if(a==4||a==5)
+		{
+			printf("*\n");
+		}
+	}
+}

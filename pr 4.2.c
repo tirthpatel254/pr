4798c489559b80d45afba5d6c7a8bfbef012c563
tirthpatel254 <heteshpatel17@gmail.com>
2025-05-27 15:37:02 +0530

    Add files via upload

---
+++ pr 4.2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,count=11;
+	
+	for(a=1;a<=4;a++)
+	{
+		for(b=1;b<=a;b++)
+		{
+			printf(" %d",count);
+			count++;
+			
+		}
+		
+			
+		printf("\n");
+    }
+   
+}
+/*
+
+
+
+
+
+*/
